bc60a0885a11a0036f7cfd7e6812f7bb6631a8cb
louis yu <yf_hello@163.com>
2019-04-12 17:51:35 +0800

    update App

--- app.go
+++ app.go
@@ -1,32 +1,74 @@
 package maru
 
-type appEnv int
+import (
+	"fmt"
+)
+
+type AppEnv int
 
 const (
-	PRODUCTION appEnv = iota + 1
+	PRODUCTION AppEnv = iota + 1
 	STAGING
 	TESTING
 	DEVELOPMENT
 )
 
+var (
+	envStrMapping = map[AppEnv]string {
+		PRODUCTION : "production",
+		STAGING : "staging",
+		TESTING : "testing",
+		DEVELOPMENT: "development",
+	}
+)
+
 type App struct {
 	isInit bool
-	env appEnv
+	env AppEnv
 	envString string
 	configFile string
 	config string
-	appName string
+	name string
 }
 
-func NewApp(env string, appName string) *App {
+func NewApp(envString string, appName string) *App {
+	env := strToEnv(envString)
+	envStr := envStrMapping[env]
 	return &App{
 		isInit: false,
-		env: PRODUCTION,
-		envString: "production",
+		env: env,
+		envString: envStr,
+		name: appName,
 	}
+}
+
+//implement string interface
+func (this *App) String() string {
+	return fmt.Sprintf("App[name=%s, env=%s]", this.name, this.envString)
+}
+
+//get App.env
+func (this *App) Env() AppEnv {
+	return this.env
+}
+
+//get App.envString
+func (this *App) EnvString() string {
+	return this.envString
+}
+
+func strToEnv(envString string) AppEnv {
+	env := PRODUCTION
+	for k, v := range envStrMapping {
+		if(v == envString){
+			env = k
+			break
+		}
+	}
+	return env
 }
 
 func (this *App) Init() {
 	this.isInit = true
-	println("app is init...")
+	println("app is init... %s", this)
 }
