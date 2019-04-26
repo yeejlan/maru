544e0a63caa1459ef699078a3398b07d5ddf03ce
louis yu <yf_hello@163.com>
2019-04-26 13:12:51 +0800

    add json.go

--- global.go
+++ global.go
@@ -1,29 +1,7 @@
 package maru
 
-import (
-	"encoding/json"
-	"strings"
-)
 
 var(
 	//Set with go build -ldflags "-X github.com/yeejlan/maru.BuildDir=xxx"
 	BuildDir string
-
-	JsonEncode = json.Marshal
-	JsonDecode = json.Unmarshal
 )
-
-//decode json to map[string]interface{}, int as json.Number
-func JsonDecodeToMap(data string) (map[string]interface{}, error) {
-	d := json.NewDecoder(strings.NewReader(data))
-	d.UseNumber()
-	var x interface{}
-	if err := d.Decode(&x); err != nil {
-		return nil, err
-	}
-	m, ok := x.(map[string]interface{})
-	if !ok {
-		return nil, NewError("json convert to map error")
-	}
-	return m, nil
-}
