430c514ad1020451043e73eaeb8cd8ab5d33cf7b
louis yu <yf_hello@163.com>
2019-05-08 09:53:38 +0800

    remove unused code

--- router.go
+++ router.go
@@ -150,17 +150,6 @@ func (this *Router) callAction(ctx *Ctx, controller string, action string) {
 				return
 			}
 			errStr := fmt.Sprintf("%#v\n", e)
-			/*
-			for i := 1; ; i += 1 {
-				pc, file, line, ok := runtime.Caller(i)
-				if !ok {
-					break
-				}
-				file = strings.TrimPrefix(file, BuildDir)
-				funcName := runtime.FuncForPC(pc).Name()
-				errStr += fmt.Sprintf("\r\n%s(): %s: %d", funcName, file, line)
-			}
-			*/
 			errStr += string(debug.Stack())
 			ctx.Error = errStr
 			internalServerError(ctx)
